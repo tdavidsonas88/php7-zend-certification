8d4342cffcbd9163d08d6d398bdc7066b030f3b9
Tadas <tdavidsonas88@gmail.com>
2020-08-29 11:44:40 +0300

    constant name with double qoutes

--- namespaced_constants.php
+++ namespaced_constants.php
@@ -4,7 +4,7 @@ namespace Foo;
 const AVOCADO = 6.02214086;
 // using define() will generate warning
 // PHP Warning:  Use of undefined constant MOLE - assumed 'MOLE' (this will throw an Error in a future version of PHP)
-define(MOLE, 'hill');
+define("MOLE", 'hill');
 
 namespace Bar;
 echo \Foo\AVOCADO;
