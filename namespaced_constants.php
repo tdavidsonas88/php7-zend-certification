ffe633098307181b13de0b506fa61a5d4d54b7b1
Tadas <tdavidsonas88@gmail.com>
2020-08-29 11:39:31 +0300

    namespaced constants should be used with const keyword to avoid warnings and errors

---
+++ namespaced_constants.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Foo;
+const AVOCADO = 6.02214086;
+// using define() will generate warning
+// PHP Warning:  Use of undefined constant MOLE - assumed 'MOLE' (this will throw an Error in a future version of PHP)
+define(MOLE, 'hill');
+
+namespace Bar;
+echo \Foo\AVOCADO;
+// PHP Fatal error:  Uncaught Error: Undefined constant 'Foo\MOLE'
+echo \Foo\MOLE;
