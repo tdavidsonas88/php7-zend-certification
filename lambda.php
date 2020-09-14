bfc90eb0567a60a344d877121f15cbe8b475fb87
Tadas <tdavidsonas88@gmail.com>
2020-09-14 21:25:07 +0300

    lambda and closure

---
+++ lambda.php
@@ -0,0 +1,10 @@
+<?php
+
+// A lambda in PHP is an anonymous function that can be stored as a variable
+$lambda = function ($a, $b) {
+    echo $a + $b;
+};
+echo gettype($lambda); // object
+echo (int)is_callable($lambda); // 1
+echo get_class($lambda); // Closure - class to represent anonymous functions.
+
