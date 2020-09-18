3e44e23899344aa9a829d962f4c25cb4b679b96b
Tadas <tdavidsonas88@gmail.com>
2020-09-18 21:18:44 +0300

    heredoc sample

---
+++ heredoc.php
@@ -0,0 +1,9 @@
+<?php
+
+echo <<<HEREDOC
+    This is a heredoc string, note:
+    1) the capitalization of the tag
+    2) the tag name foolows variable with naming rules
+    3) where the closing tag is
+HEREDOC;
+
