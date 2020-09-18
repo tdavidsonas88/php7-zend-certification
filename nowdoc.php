e8dac6bb4006da74c7cac966813e434e85375879
Tadas <tdavidsonas88@gmail.com>
2020-09-18 21:21:53 +0300

    nowdoc

---
+++ nowdoc.php
@@ -0,0 +1,9 @@
+<?php
+
+echo <<<'NOWDOC'
+This is a nowdoc string, note:
+    1) Single quotes around the label
+    2) Variables will not be evaluated
+    3) Control characters will not be evaluated
+NOWDOC;
+
